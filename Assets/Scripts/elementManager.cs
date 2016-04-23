0e8b4e39706721179ad8c4fdbb919f7f61c3086e
martymarkenson <martyamark@gmail.com>
2016-04-22 20:06:30 -0500

    Added atomSelected bool so that atoms dont continue to generate if clicked

--- Assets/Scripts/elementManager.cs
+++ Assets/Scripts/elementManager.cs
@@ -8,9 +8,11 @@ public class elementManager : MonoBehaviour {
 	public Text elementText;
 	public Rigidbody atomPrefab;
 	private Transform element;
+	public bool atomSelected;
 	// Use this for initialization
 	void Start () {
 		element = GetComponent<Transform> ();
+		atomSelected = false;
 	}
 	
 	// Update is called once per frame
@@ -27,9 +29,12 @@ public class elementManager : MonoBehaviour {
 		elementText.text = elementName;
 	}
 
-	public void clickElement(){
-		Rigidbody atomInstance;
-		atomInstance = Instantiate(atomPrefab, element.position, element.rotation) as Rigidbody;
+	public void selectElement(){
+		if (atomSelected == false) {
+			Rigidbody atomInstance;
+			atomInstance = Instantiate (atomPrefab, element.position, element.rotation) as Rigidbody;
+			atomSelected = true;
+		}
 	}
 
 }
