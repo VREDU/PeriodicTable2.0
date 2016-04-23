e751e95f1e9db6f55c6d9ba11d1ae0e9c3addb08
martymarkenson <martyamark@gmail.com>
2016-04-22 19:50:41 -0500

    First Commit

---
+++ Assets/Scripts/elementManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class elementManager : MonoBehaviour {
+	public int atomicNumber;
+	public string elementName;
+	public Text elementText;
+	public Rigidbody atomPrefab;
+	private Transform element;
+	// Use this for initialization
+	void Start () {
+		element = GetComponent<Transform> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public int getAtomicNumber() {
+		return atomicNumber;
+	}
+
+	public void SetGazedAt(bool gazedAt) {
+		GetComponent<Renderer>().material.color = gazedAt ? Color.green : Color.white;
+		elementText.text = elementName;
+	}
+
+	public void clickElement(){
+		Rigidbody atomInstance;
+		atomInstance = Instantiate(atomPrefab, element.position, element.rotation) as Rigidbody;
+	}
+
+}
---
+++ Assets/Scripts/elementNameManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class elementNameManager : MonoBehaviour {
+	Text elementName;
+
+	void Start () {
+		elementName = GetComponent<Text> ();
+		elementName.text = "";
+	}
+
+	public void Hydrogen() {
+		elementName.text = "Hydrogen";
+	}
+
+	public void Helium() {
+		elementName.text = "Helium";
+	}
+		
+}
