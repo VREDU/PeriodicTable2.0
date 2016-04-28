e42d510584d89bfae2ead4405c2f18e7c6fb716f
martymarkenson <martyamark@gmail.com>
2016-04-28 11:41:59 -0500

    Cleared up public and private vars in landingManager

--- Assets/Scripts/landingManager.cs
+++ Assets/Scripts/landingManager.cs
@@ -3,21 +3,19 @@ using System.Collections;
 
 public class landingManager : MonoBehaviour {
 	public GameObject landingPoint;
-	public Collider atom;
 	public float speed;
+	private Collider atom;
 	private float step;
-	public bool trigger;
+	private bool trigger;
 	void Start () {
 		trigger = false;
 	}
-	
-	// Update is called once per frame
+
 	void Update () {
 		step = speed * Time.deltaTime;
 		if (trigger == true) {
 			atom.transform.position = Vector3.MoveTowards (atom.transform.position, landingPoint.transform.position, step);
 		}
-
 	}
 
 	void OnTriggerEnter (Collider other) {
