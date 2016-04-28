d235b817d47e2374af6ea491afc014bedc63d996
martymarkenson <martyamark@gmail.com>
2016-04-28 11:27:44 -0500

    changed collider to atom and made speed at landing zone public

--- Assets/Scripts/landingManager.cs
+++ Assets/Scripts/landingManager.cs
@@ -3,18 +3,27 @@ using System.Collections;
 
 public class landingManager : MonoBehaviour {
 	public GameObject landingPoint;
-
+	public Collider atom;
+	public float speed;
+	private float step;
+	public bool trigger;
 	void Start () {
+		trigger = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		step = speed * Time.deltaTime;
+		if (trigger == true) {
+			atom.transform.position = Vector3.MoveTowards (atom.transform.position, landingPoint.transform.position, step);
+		}
 
 	}
 
 	void OnTriggerEnter (Collider other) {
 		Debug.Log (other.gameObject.name);
 		raycastTest.gazeMovement = false;
-		other.gameObject.transform.position = landingPoint.transform.position;
+		atom = other;
+		trigger = true;
 	}
 }
