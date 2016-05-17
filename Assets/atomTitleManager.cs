2baa4554669874b42464d56b0cf5316ac9456b7d
martymarkenson <martyamark@gmail.com>
2016-05-17 13:01:58 -0500

    Added atomTitleManager for title to always face camera

---
+++ Assets/atomTitleManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class atomTitleManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.rotation = Quaternion.LookRotation(transform.position - Camera.main.transform.position);
+	
+	}
+}
