e90e1cf671c378a9567643b5825381bdca77e7c0
martymarkenson <martyamark@gmail.com>
2016-05-19 14:49:58 -0500

    Blender hydrogen model with title is now a working background atom

--- Assets/atomTitleManager.cs
+++ Assets/atomTitleManager.cs
@@ -5,12 +5,12 @@ public class atomTitleManager : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		//transform.position = atom.transform.position.normalized;
 		transform.rotation = Quaternion.LookRotation(transform.position - Camera.main.transform.position);
-	
+		//transform.position = transform.position + Camera.main.transform.position.normalized * -1;
 	}
 }
