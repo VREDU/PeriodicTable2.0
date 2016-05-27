79180004aa0280375a978aa3b39618c6119ca926
martymarkenson <martyamark@gmail.com>
2016-05-27 12:11:44 -0500

    Added basic start screen

---
+++ Assets/periodSelector.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class periodSelector : MonoBehaviour {
+	public GameObject p1;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void period1Hover(bool gazedAt) {
+		p1.SetActive (gazedAt);
+	}
+
+	public void period1Select() {
+		SceneManager.LoadScene(1);
+	}
+}
