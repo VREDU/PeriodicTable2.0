224303974817c2a18605bd99cd31a9cde251f0e8
martymarkenson <martyamark@gmail.com>
2016-05-31 11:16:13 -0500

    Added some comments

--- Assets/Scripts/canvasManager.cs
+++ Assets/Scripts/canvasManager.cs
@@ -3,52 +3,50 @@ using System.Collections;
 using UnityEngine.SceneManagement;
 
 public class canvasManager : MonoBehaviour {
-	public GameObject congratsBackground, scoreTitle, timeTitle, instructionsTop, instructionsBottom, tryAgain;
-	public static bool gameStart;
-	public static int score;
-	public static float timeLeft;
-
-	Animator anim;
+	public static bool playing; //flag for if user is in a round
+	public static float timeLeft; //timeLeft before the round is over
+	public static int score; //users score
+	private int goal; //the score the user needs to obtain
+	Animator anim; //controls when instructions, time, score and playagain/next level
 
 	void Start () {
+		goal = 5;
 		timeLeft = 60f;
 		anim = GetComponent<Animator> ();
 	}
 
 	void Update () {
 		Debug.Log (timeLeft);
-		if (gameStart) {
+		if (playing) {
 			timeLeft -= Time.deltaTime;
-			anim.SetTrigger ("playing");
+			anim.SetTrigger ("playing"); //score and timeLeft are shown to user
 		}
 
-		if (score > 4) {
+		if (score >= goal) {
 			anim.SetTrigger ("gameOver");
+			playing = false;
 		}
 
 		if (timeLeft<=0) {
 			anim.SetTrigger ("gameOver");
+			playing = false;
 		}
 	}
 
 	public void playAgain() {
 		SceneManager.LoadScene(0);
 	}
-
-	public void startGame() {
-		score = 0;
-	}
-
+		
 	public void home() {
 		SceneManager.LoadScene (0);
 	}
 
+	//if won go to next level else try again
 	public void nextSteps() {
-		if (score > 4) {
+		if (score >= goal) {
 			SceneManager.LoadScene (2);
 		} else {
 			SceneManager.LoadScene (1);
-			gameStart = false;
 		}
 	}
 }
--- Assets/Scripts/elementManager.cs
+++ Assets/Scripts/elementManager.cs
@@ -3,14 +3,14 @@ using System.Collections;
 using UnityEngine.UI;
 
 public class elementManager : MonoBehaviour {
-	public string elementName;
 	public GameObject atomPrefab, atoms_boundary;
+	public string elementName;
 	private Transform element;
 	private Vector3 offset;
 
 	void Start () {
 		element = GetComponent<Transform> ();
-		offset = new Vector3 (0, 0, -1);
+		offset = new Vector3 (0, 0, -1); //spawns atom in front of element
 	}
 		
 	void Update () {
@@ -21,9 +21,9 @@ public class elementManager : MonoBehaviour {
 	}
 
 	public void selectElement() {
-		//change this shit
-		if (canvasManager.gameStart == false) {
-			canvasManager.gameStart = true;
+
+		if (canvasManager.playing == false) {
+			canvasManager.playing = true;
 		}
 			GameObject.FindGameObjectWithTag ("gazeObjectHolder").GetComponent<gazeMovementManager> ().createGazeObject ();
 			Instantiate (atomPrefab, element.position + offset, element.rotation);
