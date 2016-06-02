a188f0809c935ab4f2f8cf27b8e6387751c8deb9
martymarkenson <martyamark@gmail.com>
2016-06-02 17:42:54 -0500

    Added persistant SaveLoad script

--- Assets/Scripts/SaveLoad.cs
+++ Assets/Scripts/SaveLoad.cs
@@ -1,6 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public static class SaveLoad {
+public  class SaveLoad:MonoBehaviour {
 
+	public static SaveLoad saveLoad;
+	public int currentLevel;
+
+	void Awake() {
+		if (saveLoad == null) {
+			DontDestroyOnLoad (gameObject);
+			saveLoad = this;
+		} else if (saveLoad != this){
+			Destroy (gameObject);
+		}
+	}
 }
